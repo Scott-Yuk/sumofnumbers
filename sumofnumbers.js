0ff89d3bc1dbe999b8c97b65ac6c612ad5492835
Scott Yuk <syuk@hawaii.edu>
2022-02-06 15:30:48 -1000

    E26

---
+++ sumofnumbers.js
@@ -0,0 +1,38 @@
+const testNums = [1, 2, 3, 4];
+
+function sumFor(nums) {
+  let total = 0;
+  for (const num of nums) {
+    total += num;
+  }
+  return total;
+}
+
+console.log(sumFor(testNums));
+
+function sumWhile(num) {
+  let total = 0;
+  let trav = 0;
+  while (trav < num.length) {
+    total += num[trav];
+    trav++;
+  }
+  return total;
+}
+
+console.log(sumWhile(testNums));
+
+function sumRecursion(num) {
+  if (num.length === 0) {
+    return 0;
+  }
+  return num[0] + sumRecursion(num.slice(1, num.length));
+}
+
+console.log(sumRecursion(testNums));
+
+function sumTheSimpleWay(num) {
+  return _.reduce(num, function(memo, nums){ return memmo + nums; }, 0);
+}
+
+console.log(sumTheSimpleWay(testNums));
